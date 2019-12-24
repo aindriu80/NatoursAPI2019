f715b7ddd211c75f233e5d3d09464cb4beb80884
unknown <aindriu80@gmail.com>
2019-12-24 09:58:33 +0000

    Creating documents &  testing the model

--- server.js
+++ server.js
@@ -38,6 +38,19 @@ const tourSchema = new mongoose.Schema({
   }
 });
 const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+});
+
+testTour
+  .save().then(doc => {
+    console.log(doc);
+  }).catch(err => {
+    console.log('Error: ', err)
+  })
+
 // 4 -> Start Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
