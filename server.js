3a8f07711f358fd1948f386ac870e3da9157932f
unknown <aindriu80@gmail.com>
2019-12-25 10:00:12 +0000

    refactoring for MVC

--- server.js
+++ server.js
@@ -22,35 +22,6 @@ mongoose
   })
   .then(() => console.log('DB connection successful'));
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a price'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Park Camper',
-  price: 997
-});
-
-testTour
-  .save().then(doc => {
-    console.log(doc);
-  }).catch(err => {
-    console.log('Error: ', err)
-  })
-
 // 4 -> Start Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
