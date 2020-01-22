2f9edea19092da2ae112d59c3de4d4c1e2a86b59
unknown <aindriu80@gmail.com>
2020-01-22 17:40:10 +0000

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION :S Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({
   path: './config.env'
 });
@@ -31,8 +37,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION :S Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
   });
