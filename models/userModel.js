db002f53120bf5a1eb773f9584f3f963ecc6c00c
unknown <aindriu80@gmail.com>
2020-02-06 16:50:12 +0000

    Deleting the Current User

--- models/userModel.js
+++ models/userModel.js
@@ -40,7 +40,12 @@ const userSchema = new mongoose.Schema({
   },
   passwordChangedAt: Date,
   passwordResetToken: String,
-  passwordResetExpires: Date
+  passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false
+  }
 });
 
 userSchema.pre('save', async function(next) {
@@ -62,6 +67,11 @@ userSchema.pre('save', function(next) {
   next();
 });
 
+userSchema.pre(/^find/, function(next) {
+  // this points to the current query
+  this.find({ active: { $ne: false } });
+  next();
+});
 userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
