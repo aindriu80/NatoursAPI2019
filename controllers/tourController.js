4a7554f79794bc0fc589405c4e19a612710c00bd
unknown <aindriu80@gmail.com>
2020-03-04 17:30:20 +0000

    Geospatial Queries: Tours within Radius - the query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -95,10 +95,10 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
 // //tours-within?distance=233&center=-40,45,unit=mi
 // //tours-within/233/center/34.111745,-118.113491/unit/mi
 
-exports.getToursWithin = (req, res, next) => {
+exports.getToursWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
-
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
   if (!lat || !lng) {
     next(
       new AppError(
@@ -107,8 +107,14 @@ exports.getToursWithin = (req, res, next) => {
       )
     );
   }
-  console.log(distance, lat, lng, unit);
-  res.status(200).json({
-    status: 'success'
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } }
   });
-};
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: tours
+    }
+  });
+});
