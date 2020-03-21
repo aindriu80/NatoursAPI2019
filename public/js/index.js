5e31f8116c1f551636229f07b8a932732965d944
unknown <aindriu80@gmail.com>
2020-03-21 11:46:07 +0000

    Updating User Data with API

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,14 @@
 import '@babel/polyfill';
 import { displayMap} from './mapbox';
 import {login, logout} from './login';
+import { updateData} from'./updateSettings';
 
 // DOM Elements
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+
 // Delegation
 if (mapBox){
     const locations = JSON.parse(mapBox.dataset.locations);
@@ -23,3 +26,11 @@ if (loginForm)
 });
 
 if (logOutBtn) logOutBtn.addEventListener('click', logout);
+
+if (userDataForm)
+    userDataForm.addEventListener('submit', e=>{
+        e.preventDefault();
+        const name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        updateData(name, email);
+    });
